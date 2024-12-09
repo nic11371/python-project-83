32fdf748787b527990faff7c4a3269b08c03e4bb
Nic11371 <nic11371@gmail.com>
2024-12-09 20:31:23 +0300

    connect_bootstrap

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,13 @@
 import os
-from flask import Flask
+from flask import (
+    # get_flashed_messages,
+    # flash,
+    Flask,
+    # redirect,
+    render_template,
+    # request,
+    # url_for
+)
 from dotenv import load_dotenv
 
 
@@ -10,4 +18,6 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def root():
-    return "Welcome to Flask"
+    return render_template(
+        'pages/index.html'
+    )
